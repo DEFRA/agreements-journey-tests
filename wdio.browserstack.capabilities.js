4f22016aaef769912162a8340862fbb55e856fc1
Arijit Ghosh <arijit.ghosh@equalexperts.com>
2025-09-24 11:33:25 +0100

    updated browser stack proxy details

--- wdio.browserstack.capabilities.js
+++ wdio.browserstack.capabilities.js
@@ -26,94 +26,18 @@ export const browserStackCapabilities = [
       osVersion: '11'
     }
   },
+  // macOS
   {
-    browserName: 'Firefox',
+    browserName: 'Chrome',
     'wdio-ics:options': {
-      logName: 'win-firefox'
+      logName: 'osx-chrome'
     },
     'bstack:options': {
       idleTimeout: 300,
       resolution: '1920x1080',
       browserVersion: 'latest',
-      os: 'Windows',
-      osVersion: '11'
+      os: 'OS X',
+      osVersion: 'Sonoma' // Changed from 'Sequoia'
     }
-  } //,
-  // // macOS
-  // {
-  //   browserName: 'Chrome',
-  //   'wdio-ics:options': {
-  //     logName: 'osx-chrome'
-  //   },
-  //   'bstack:options': {
-  //     idleTimeout: 300,
-  //     resolution: '1920x1080',
-  //     browserVersion: 'latest',
-  //     os: 'OS X',
-  //     osVersion: 'Sequoia'
-  //   }
-  // },
-  // {
-  //   browserName: 'Firefox',
-  //   'wdio-ics:options': {
-  //     logName: 'osx-firefox'
-  //   },
-  //   'bstack:options': {
-  //     idleTimeout: 300,
-  //     resolution: '1920x1080',
-  //     browserVersion: 'latest',
-  //     os: 'OS X',
-  //     osVersion: 'Sequoia'
-  //   }
-  // },
-  // // iOS
-  // {
-  //   browserName: 'Chrome',
-  //   'wdio-ics:options': {
-  //     logName: 'ios-chrome'
-  //   },
-  //   'bstack:options': {
-  //     idleTimeout: 300,
-  //     deviceOrientation: 'portrait',
-  //     deviceName: 'iPhone 16',
-  //     osVersion: '18'
-  //   }
-  // },
-  // {
-  //   browserName: 'Safari',
-  //   'wdio-ics:options': {
-  //     logName: 'ios-safari'
-  //   },
-  //   'bstack:options': {
-  //     idleTimeout: 300,
-  //     deviceOrientation: 'portrait',
-  //     deviceName: 'iPhone 16',
-  //     osVersion: '18'
-  //   }
-  // },
-  // // android
-  // {
-  //   browserName: 'Chrome',
-  //   'wdio-ics:options': {
-  //     logName: 'android-chrome'
-  //   },
-  //   'bstack:options': {
-  //     idleTimeout: 300,
-  //     deviceOrientation: 'portrait',
-  //     deviceName: 'Samsung Galaxy S23',
-  //     osVersion: '13.0'
-  //   }
-  // },
-  // {
-  //   browserName: 'Samsung',
-  //   'wdio-ics:options': {
-  //     logName: 'android-samsung'
-  //   },
-  //   'bstack:options': {
-  //     idleTimeout: 300,
-  //     deviceOrientation: 'portrait',
-  //     deviceName: 'Samsung Galaxy S23',
-  //     osVersion: '13.0'
-  //   }
-  // }
+  }
 ]
