4df6e765e3a07dbb0f86523ccb98cc61d10288e2
Arijit Ghosh <arijit.ghosh@equalexperts.com>
2025-12-08 14:58:01 +0000

    updated

--- test/specs/alpha.e2e.js
+++ test/specs/alpha.e2e.js
@@ -3,9 +3,9 @@ import { ReviewOfferPage } from '../page-objects/review-offer.page.js'
 import { AcceptYourOfferPage } from '../page-objects/accept-your-offer.page.js'
 import { OfferAcceptedPage } from '../page-objects/offer-accepted.page.js'
 import { getAgreement } from '../services/get-agreement.js'
-import { unacceptAgreement } from '../services/unaccept-agreement.js'
 import { createTestAgreement } from '../support/agreement-helper.js'
-import * as constants from '../support/constants.js'
+// import { unacceptAgreement } from '../services/unaccept-agreement.js'
+// import * as constants from '../support/constants.js'
 import { LoginPage } from '../page-objects/login.page.js'
 const reviewOfferPage = new ReviewOfferPage()
 const acceptYourOfferPage = new AcceptYourOfferPage()
@@ -43,31 +43,34 @@ describe('E2E: Create, Accept,Un-accept and validate agreement', () => {
     expect(agreementData.invoice[0].invoiceNumber).toBeDefined()
   })
 
-  it('should un-accept the agreement and validate via API', async () => {
-    await unacceptAgreement(agreementId)
-    const agreementData = await getAgreement(agreementId)
-    console.log(
-      'agreementData after unaccept:-----',
-      JSON.stringify(agreementData, null, 2)
-    )
-    expect(agreementData.status).toBe('offered')
-  })
-
-  it('re-acceptance of agreement and validate via API', async () => {
-    await reviewOfferPage.open()
-    await reviewOfferPage.selectContinue()
-    await acceptYourOfferPage.clickConfirmCheckbox()
-    await acceptYourOfferPage.selectAcceptOffer()
-    const confirmationText = await offerAcceptedPage.getConfirmationText()
-    expect(confirmationText).toBe(constants.OFFER_ACCEPTED_HEADER)
-    const agreementData = await getAgreement(agreementId)
-    console.debug(
-      'agreementData after accept:-----',
-      JSON.stringify(agreementData, null, 2)
-    )
-    expect(agreementData).not.toBeUndefined()
-    expect(agreementData.status).toBe('accepted')
-    expect(agreementData.invoice).toBeDefined()
-    expect(agreementData.invoice[0].invoiceNumber).toBeDefined()
-  })
+  /*
+    Un accept and re-acceptance not part of scope will be revisited
+   */
+  // it('should un-accept the agreement and validate via API', async () => {
+  //   await unacceptAgreement(agreementId)
+  //   const agreementData = await getAgreement(agreementId)
+  //   console.log(
+  //     'agreementData after unaccept:-----',
+  //     JSON.stringify(agreementData, null, 2)
+  //   )
+  //   expect(agreementData.status).toBe('offered')
+  // })
+  //
+  // it('re-acceptance of agreement and validate via API', async () => {
+  //   await reviewOfferPage.open()
+  //   await reviewOfferPage.selectContinue()
+  //   await acceptYourOfferPage.clickConfirmCheckbox()
+  //   await acceptYourOfferPage.selectAcceptOffer()
+  //   const confirmationText = await offerAcceptedPage.getConfirmationText()
+  //   expect(confirmationText).toBe(constants.OFFER_ACCEPTED_HEADER)
+  //   const agreementData = await getAgreement(agreementId)
+  //   console.debug(
+  //     'agreementData after accept:-----',
+  //     JSON.stringify(agreementData, null, 2)
+  //   )
+  //   expect(agreementData).not.toBeUndefined()
+  //   expect(agreementData.status).toBe('accepted')
+  //   expect(agreementData.invoice).toBeDefined()
+  //   expect(agreementData.invoice[0].invoiceNumber).toBeDefined()
+  // })
 })
