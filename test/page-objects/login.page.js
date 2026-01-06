22bac5d47f75962b1352595c81b7d3b217ad173a
Arijit Ghosh <arijit.ghosh@equalexperts.com>
2026-01-06 16:44:10 +0000

    updated login page

--- test/page-objects/login.page.js
+++ test/page-objects/login.page.js
@@ -5,27 +5,25 @@ import { genAuthToken } from '../support/gen-auth-token.js'
 class LoginPage extends Page {
   async login(sbi) {
     let path = browser.options.proxy
+    const isParallelRun = process.env.PARALLEL_RUN === 'true'
 
-    if (sbi) {
+    if (isParallelRun) {
       browser.options.baseUrl = browser.options.unproxiedUrl
       browser.options.proxy = '/'
-
       const authToken = sbi ? genAuthToken({ sbi }) : ''
       const separator = browser.options.proxy.includes('?') ? '&' : '?'
       path = `${browser.options.proxy}${separator}x-encrypted-auth=${authToken}`
-    }
-
-    console.debug(`URL set to: ${browser.options.baseUrl}${path}`)
-    this.open(path)
-
-    if (sbi) {
+      console.debug(`URL set to: ${browser.options.baseUrl}${path}`)
+      this.open(path)
       return
     }
 
+    // Normal username/password login
+    console.debug(`URL set to: ${browser.options.baseUrl}${path}`)
+    this.open(path)
     const usernameInput = await $('#crn')
     const passwordInput = await $('#password')
     const submitButton = await $('button[type="submit"]')
-
     await usernameInput.setValue(constants.USERNAME)
     await passwordInput.setValue(constants.PASSWORD)
     await submitButton.click()
