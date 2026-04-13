db425d76f6dd80cf3470191b4bdf078514da222e
Arijit Ghosh <arijit.ghosh@equalexperts.com>
2026-04-13 11:44:57 +0100

    updated

--- wdio.browserstack.capabilities.all.js
+++ wdio.browserstack.capabilities.all.js
@@ -1,10 +1,29 @@
 export const browserStackCapabilities = [
-  // windows
   {
     browserName: 'Chrome',
-    'wdio-ics:options': {
-      logName: 'win-chrome'
-    },
+    'wdio-ics:options': { logName: 'win-chrome' },
+    'bstack:options': {
+      idleTimeout: 300,
+      resolution: '1920x1080',
+      browserVersion: 'latest',
+      os: 'Windows',
+      osVersion: '11'
+    }
+  },
+  {
+    browserName: 'Chrome',
+    'wdio-ics:options': { logName: 'win-chrome-latest-3' },
+    'bstack:options': {
+      idleTimeout: 300,
+      resolution: '1920x1080',
+      browserVersion: 'latest-3',
+      os: 'Windows',
+      osVersion: '11'
+    }
+  },
+  {
+    browserName: 'Edge',
+    'wdio-ics:options': { logName: 'win-edge' },
     'bstack:options': {
       idleTimeout: 300,
       resolution: '1920x1080',
@@ -15,9 +34,18 @@ export const browserStackCapabilities = [
   },
   {
     browserName: 'Edge',
-    'wdio-ics:options': {
-      logName: 'win-edge'
-    },
+    'wdio-ics:options': { logName: 'win-edge-latest-3' },
+    'bstack:options': {
+      idleTimeout: 300,
+      resolution: '1920x1080',
+      browserVersion: 'latest-3',
+      os: 'Windows',
+      osVersion: '11'
+    }
+  },
+  {
+    browserName: 'Firefox',
+    'wdio-ics:options': { logName: 'win-firefox' },
     'bstack:options': {
       idleTimeout: 300,
       resolution: '1920x1080',
@@ -28,36 +56,62 @@ export const browserStackCapabilities = [
   },
   {
     browserName: 'Firefox',
-    'wdio-ics:options': {
-      logName: 'win-firefox'
-    },
+    'wdio-ics:options': { logName: 'win-firefox-latest-3' },
     'bstack:options': {
       idleTimeout: 300,
       resolution: '1920x1080',
-      browserVersion: 'latest',
+      browserVersion: 'latest-3',
       os: 'Windows',
       osVersion: '11'
     }
   },
-  // macOS
   {
     browserName: 'Chrome',
-    'wdio-ics:options': {
-      logName: 'osx-chrome'
-    },
+    'wdio-ics:options': { logName: 'osx-chrome' },
     'bstack:options': {
       idleTimeout: 300,
       resolution: '1920x1080',
       browserVersion: 'latest',
       os: 'OS X',
-      osVersion: 'Sequoia' // Updated from 'Sonoma'
+      osVersion: 'Sequoia'
+    }
+  },
+  {
+    browserName: 'Chrome',
+    'wdio-ics:options': { logName: 'osx-chrome-latest-3' },
+    'bstack:options': {
+      idleTimeout: 300,
+      resolution: '1920x1080',
+      browserVersion: 'latest-3',
+      os: 'OS X',
+      osVersion: 'Sequoia'
     }
   },
   {
     browserName: 'Safari',
-    'wdio-ics:options': {
-      logName: 'osx-safari'
-    },
+    'wdio-ics:options': { logName: 'osx-safari' },
+    'bstack:options': {
+      idleTimeout: 300,
+      resolution: '1920x1080',
+      browserVersion: 'latest',
+      os: 'OS X',
+      osVersion: 'Sequoia'
+    }
+  },
+  {
+    browserName: 'Safari',
+    'wdio-ics:options': { logName: 'osx-safari-latest-3' },
+    'bstack:options': {
+      idleTimeout: 300,
+      resolution: '1920x1080',
+      browserVersion: 'latest-3',
+      os: 'OS X',
+      osVersion: 'Sequoia'
+    }
+  },
+  {
+    browserName: 'Firefox',
+    'wdio-ics:options': { logName: 'osx-firefox' },
     'bstack:options': {
       idleTimeout: 300,
       resolution: '1920x1080',
@@ -68,23 +122,18 @@ export const browserStackCapabilities = [
   },
   {
     browserName: 'Firefox',
-    'wdio-ics:options': {
-      logName: 'osx-firefox'
-    },
+    'wdio-ics:options': { logName: 'osx-firefox-latest-3' },
     'bstack:options': {
       idleTimeout: 300,
       resolution: '1920x1080',
-      browserVersion: 'latest',
+      browserVersion: 'latest-3',
       os: 'OS X',
       osVersion: 'Sequoia'
     }
   },
-  // iPhone
   {
     browserName: 'Safari',
-    'wdio-ics:options': {
-      logName: 'ios-safari'
-    },
+    'wdio-ics:options': { logName: 'ios-safari' },
     'bstack:options': {
       idleTimeout: 300,
       deviceName: 'iPhone 14',
@@ -94,10 +143,19 @@ export const browserStackCapabilities = [
     }
   },
   {
+    browserName: 'Safari',
+    'wdio-ics:options': { logName: 'ios-safari-older' },
+    'bstack:options': {
+      idleTimeout: 300,
+      deviceName: 'iPhone 13',
+      osVersion: '15',
+      realMobile: true,
+      browserVersion: 'latest'
+    }
+  },
+  {
     browserName: 'Chrome',
-    'wdio-ics:options': {
-      logName: 'ios-chrome'
-    },
+    'wdio-ics:options': { logName: 'ios-chrome' },
     'bstack:options': {
       idleTimeout: 300,
       deviceName: 'iPhone 15',
@@ -106,12 +164,42 @@ export const browserStackCapabilities = [
       browserVersion: 'latest'
     }
   },
-  // Android
   {
     browserName: 'Chrome',
-    'wdio-ics:options': {
-      logName: 'android-chrome'
-    },
+    'wdio-ics:options': { logName: 'ios-chrome-older' },
+    'bstack:options': {
+      idleTimeout: 300,
+      deviceName: 'iPhone 13',
+      osVersion: '15',
+      realMobile: true,
+      browserVersion: 'latest'
+    }
+  },
+  {
+    browserName: 'Chrome',
+    'wdio-ics:options': { logName: 'android-chrome' },
+    'bstack:options': {
+      idleTimeout: 300,
+      deviceName: 'Samsung Galaxy S23',
+      osVersion: '13.0',
+      realMobile: true,
+      browserVersion: 'latest'
+    }
+  },
+  {
+    browserName: 'Chrome',
+    'wdio-ics:options': { logName: 'android-chrome-older' },
+    'bstack:options': {
+      idleTimeout: 300,
+      deviceName: 'Samsung Galaxy S22',
+      osVersion: '12.0',
+      realMobile: true,
+      browserVersion: 'latest'
+    }
+  },
+  {
+    browserName: 'Samsung',
+    'wdio-ics:options': { logName: 'android-samsung-internet' },
     'bstack:options': {
       idleTimeout: 300,
       deviceName: 'Samsung Galaxy S23',
@@ -122,13 +210,11 @@ export const browserStackCapabilities = [
   },
   {
     browserName: 'Samsung',
-    'wdio-ics:options': {
-      logName: 'android-samsung-internet'
-    },
+    'wdio-ics:options': { logName: 'android-samsung-internet-older' },
     'bstack:options': {
       idleTimeout: 300,
-      deviceName: 'Samsung Galaxy S23',
-      osVersion: '13.0',
+      deviceName: 'Samsung Galaxy S22',
+      osVersion: '12.0',
       realMobile: true,
       browserVersion: 'latest'
     }
